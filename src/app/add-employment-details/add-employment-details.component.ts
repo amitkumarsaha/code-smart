2e18ae335e8914f165f352d6af81083144c302bb
HardikaRathod <hardika.rathod2810@gmail.com>
2020-04-08 19:48:37 +0530

    employement details UI

---
+++ src/app/add-employment-details/add-employment-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-add-employment-details',
+  templateUrl: './add-employment-details.component.html',
+  styleUrls: ['./add-employment-details.component.scss']
+})
+export class AddEmploymentDetailsComponent implements OnInit {
+  @Input() name:any;
+  model: NgbDateStruct;
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  ngOnInit() {
+  }
+
+}
